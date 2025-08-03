8a5072bc48a16cdc05932d66d308eaeada1ee994
rajpratap29 <therajpratapofficial@gmail.com>
2025-08-03 15:11:17 +0530

    Update: Learned about mutable, immutable and data types

--- 01_Basics/hello_ji.py
+++ 01_Basics/hello_ji.py
@@ -4,3 +4,13 @@ def chai(n):
     print(n)
 
 chai("Hnji")
+
+chai_one = "Mhengi wali chai"
+chai_two = "Sasti wali chai"
+chai_three = "Masala chai"
+
+
+username = "Helwoo Ji"
+print(username)
+username = "Hupp"
+print(username)
