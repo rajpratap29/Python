7831ebacaa4ca850a4e975672a575ae52c79219c
rajpratap29 <therajpratapofficial@gmail.com>
2025-08-18 19:45:25 +0530

    Day 3 Complete: Learn about randomness and list and created a Rock, Paper, Scissors Game

---
+++ Day 04/list.py
@@ -0,0 +1,12 @@
+states_of_india = ["UP", "UK", "Haryana", "MP"]
+print(states_of_india[0])
+print(states_of_india[-1])
+
+print(states_of_india)
+
+states_of_india.append("Gujrat")
+print(states_of_india)
+
+states_of_india.extend(["Punjab", "Kerela"])
+print(states_of_india)
+
---
+++ Day 04/name_selector.py
@@ -0,0 +1,8 @@
+import random
+
+names_string = '"Raj", "Aditya", "Chintu", "Priyavarat", "Ridam"'
+names = names_string.split(", ")
+num_items = len(names)
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today!")
