426a18ded33228f1d9c0127c8254c83eee723489
rajpratap29 <therajpratapofficial@gmail.com>
2025-09-01 12:53:05 +0530

    Day 15

--- Day 15/main.py
+++ Day 15/main.py
@@ -1,4 +1,40 @@
-# Intermediate level
-# Day 15/main.py
+# Intermediate level starts from Day 15
 
-#Starting 15 to 20
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+is_on = True
+
+while is_on:
+     choice = input("What would you like? (espresso/latte/cappuccino): ")
+     if choice == "off":
+        is_on = False
