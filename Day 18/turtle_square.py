127b38f3ce82aedb6884617b609015c97e89cbaf
rajpratap29 <therajpratapofficial@gmail.com>
2025-09-04 23:25:55 +0530

    Day 18

---
+++ Day 18/turtle_square.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+screen = Screen()
+screen.exitonclick()
