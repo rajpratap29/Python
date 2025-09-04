3689e41940f933e57fc44d422abb6f216ba49661
rajpratap29 <therajpratapofficial@gmail.com>
2025-09-05 00:34:04 +0530

    Day 18: Learned turtle and created a hirst painting

---
+++ Day 18/painting.py
@@ -0,0 +1,46 @@
+# import colorgram
+
+# rgb_colors = []
+# colors = colorgram.extract('/Users/rajpratap/Desktop/Python - CAC/Day 18/imgpy.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+from turtle import Turtle, Screen, colormode
+import random
+
+tim = Turtle()
+colormode(255)
+tim.speed("fastest")
+
+color_list = [(1, 10, 30), (229, 235, 242), (239, 232, 238), (122, 95, 41), (71, 31, 21), (238, 212, 72), (220, 81, 59), (226, 117, 100), (93, 1, 21), (178, 140, 171), (151, 92, 115), (35, 90, 26), (7, 154, 72), (205, 63, 91), (221, 178, 218), (168, 129, 77), (1, 64, 147), (3, 79, 29), (4, 220, 218), (80, 135, 179), (132, 158, 177), (81, 110, 136), (116, 187, 164), (11, 215, 222), (117, 19, 37), (131, 224, 209), (230, 173, 165), (243, 205, 7)]
+
+tim.setheading(225)
+tim.penup()
+tim.hideturtle()
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.penup()
+    tim.forward(50)
+    tim.pendown()
+
+    if dot_count % 10 == 0:
+        tim.penup()
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+        tim.pendown()
+
+screen = Screen()
+screen.exitonclick( )
--- Day 18/turtle_circle.py
+++ Day 18/turtle_circle.py
@@ -8,17 +8,18 @@ def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+    color = (r, g, b)
+    return color
 
-directions = [0,90,180,270]
-tim.pensize(15)
 tim.speed("fastest")
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + 10)
+
+draw_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()
