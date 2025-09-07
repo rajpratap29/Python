c219d71cd6d5ce1819028bdda7fe4f2a2a5dddcc
rajpratap29 <therajpratapofficial@gmail.com>
2025-09-07 23:12:05 +0530

    Day 24: File Handling

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,1 +1,9 @@
-# Day 24
+with open("Day 24/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# with open("Day 24/my_file.txt", mode="a") as file:
+#     file.write("\nNew text.")
+
+with open("Day 24/new_file.txt", mode="w") as file:
+    file.write("New text.")
