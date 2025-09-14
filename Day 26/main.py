1a31ae7c215c15c9eb12d99339661966d17e12d4
rajpratap29 <therajpratapofficial@gmail.com>
2025-09-14 22:15:45 +0530

    Day 26: Learned about list and dict comprehension

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,2 +1,13 @@
-# Day 26
-# Pandas
+# List Comprehension
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+caps_names = [name.upper() for name in names if len(name) > 5]
+print(caps_names)
+
+# Dictionary Comprehension
+import random
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+student_scores = {student: random.randint(1, 100) for student in names}
+passed_students = {student: score for (student, score) in student_scores.items() if score >= 60}
+print(passed_students)
