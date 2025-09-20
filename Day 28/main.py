b1231f44de9ba11f95d16fda92e3293b005f68e6
rajpratap29 <therajpratapofficial@gmail.com>
2025-09-20 22:01:57 +0530

    Day 28: Pomodoro app

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,2 +1,18 @@
 # Day 28
 # Python
+# Pomodoro app
+
+from tkinter import *
+from tkinter import messagebox
+import math
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
