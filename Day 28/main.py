62349b751f21585ded2c21f0d6faefa4e4df53f0
rajpratap29 <therajpratapofficial@gmail.com>
2025-09-22 21:30:45 +0530

    Day 28

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,7 +1,3 @@
-# Day 28
-# Python
-# Pomodoro app
-
 from tkinter import *
 from tkinter import messagebox
 import math
@@ -17,3 +13,30 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
+
+
+
+
+# UI Setup
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="Day 28/tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+window.mainloop()
