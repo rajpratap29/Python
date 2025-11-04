315dd68795cc21de275b29940c631e8ba247008f
rajpratap29 <therajpratapofficial@gmail.com>
2025-11-04 22:40:01 +0530

    Day 35: Created Rain alert app and sending of text message

---
+++ Day 35/main.py
@@ -0,0 +1,35 @@
+import requests
+from twilio.rest import Client
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "add your own api key here"
+account_sid = "add your account sid here" 
+auth_token = "add your twilio auth token here"
+
+weather_params = {
+    "lat": 29.963659,
+    "lon": 77.546028,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) > 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+            body="It's going to rain today. Remember to bring an umbrella.",
+            from_="add your number given by twilio here",
+            to="add your verified phone number here"
+    )
+    print(message.status)
